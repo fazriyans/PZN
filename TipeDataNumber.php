2b0f7a0292512b9ef355a62451b9e5aeabe0c790
fazriyans <afazri57@gmail.com>
2021-10-14 23:01:16 +0700

    php dasar

---
+++ TipeDataNumber.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Desimal : "; 
+var_dump(1234);
+
+
+echo "Octal : "; 
+var_dump(01234);
+
+
+echo "Hexadecimal : "; 
+var_dump(0x1A);
+
+
+echo "Binary : "; 
+var_dump(0b111111);
+
+echo "Underscore in number";
+var_dump(1_241_241_241);
+
+echo "Floating Point : ";
+var_dump(1.234);
